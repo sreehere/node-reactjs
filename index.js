7ecee3aba10aedb9391a032ff5344ea2005580ca
sreehere <sreekanth.p.sreeni@gmail.com>
2020-05-12 18:39:45 -0500

    third

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/greeting', (req, res) => {
+app.get('/', (req, res) => {
   res.send({hi: 'there'});
 });
 const PORT = process.env.PORT || 5000
